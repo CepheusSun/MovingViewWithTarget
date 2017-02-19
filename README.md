080e351a6a600b5acdf5f934eb406d63c331b3a3
孙扬 <cd_sunyang@163.com>
2017-02-20 03:51:43 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MovingViewWithTarget
 iOS pieces
+
+
+![](http://ocg4av0wv.bkt.clouddn.com/targetswithanimation.jpg)
+
+暂时用一下图片
+
+
--- 移动中的View添加事件/ViewController.m
+++ 移动中的View添加事件/ViewController.m
@@ -29,7 +29,7 @@
     
     [self.view addSubview:button];
     
-    
+//    
 //    [UIView animateWithDuration:10 animations:^{
 //        //动画中事件无效
 //        button.frame = CGRectMake(0, 448, 100, 100);
@@ -37,30 +37,57 @@
 //    }];
     button.userInteractionEnabled = NO;//取消响应事件
 
-    
+//    NSLog(@"%@---%p",button.layer,button.layer);
+//    NSLog(@"%@---%p",button.layer.presentationLayer,button.layer.presentationLayer);
+//    NSLog(@"%@---%p",button.layer.modelLayer,button.layer.modelLayer);
     [UIView animateWithDuration:10.f
                           delay:0
                         options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionAllowUserInteraction animations:^{
-                            button.frame = CGRectMake(0, 448, 100, 100);
+//                            button.frame = CGRectMake(0, 448, 100, 100);
+                            button.layer.frame = CGRectMake(0, 448, 100, 100);
                         } completion:^(BOOL finished) {
-    
-                        }];//动画中事件在终点
+//                            NSLog(@"----%@---%p",button.layer,button.layer);
+//                            NSLog(@"----%@---%p",button.layer.presentationLayer,button.layer.presentationLayer);
+//                            NSLog(@"----%@---%p",button.layer.modelLayer,button.layer.modelLayer);
 
+                        }];//动画中事件在终点
+//    NSLog(@"--%@---%p",button.layer,button.layer);
+//    NSLog(@"--%@---%p",button.layer.presentationLayer,button.layer.presentationLayer);
+//    NSLog(@"--%@---%p",button.layer.modelLayer,button.layer.modelLayer);
+    
+    NSLog(@"outside animation block: %@",
+          [button actionForLayer:button.layer forKey:@"position"]);
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        NSLog(@"inside animation block: %@",
+              [button actionForLayer:button.layer forKey:@"position"]);
+    }];
+    
+//    CALayer *layer = [[CALayer alloc] init];
+//    layer.frame = CGRectMake(20, 20, 50, 50);
+//    layer.backgroundColor = [UIColor blackColor].CGColor;
+//    UIImage *image = [UIImage imageNamed:@"test.jpg"];
+//    layer.contents = (__bridge id)image.CGImage;
+//    [button.layer addSublayer:layer];
+//    
+//    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+//    imageView.frame = CGRectMake(20, 20, 50, 50);
+//    [button addSubview:imageView];
+//    imageView.userInteractionEnabled = NO;
+    
     button_ = button;
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     CGPoint point = [[touches anyObject] locationInView:self.view];
     
-    CGPoint presentPosition = [[button_.layer presentationLayer] position];
-    
-    if (point.x > presentPosition.x - 50 && point.x < presentPosition.x + 50 && point.y > presentPosition.y - 50 && point.y < presentPosition.y + 50) {
+    if (CGRectContainsPoint(button_.layer.presentationLayer.frame, point)) {
         NSLog(@"youxiangying");
         [self buttonEvent:button_];
     }
-    
 }
+
+
 - (void)buttonEvent:(UIButton*)button {
     NSLog(@"aaa");
     static int flag = 0;
